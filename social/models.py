63e2541efb41a6137ea01b37a9574aeb23abb426
pablotose <pablotoledosegundo@gmail.com>
2021-04-08 13:36:56 +0200

    Segundo Commit , Creacion de los modelos y actualizacion de las páginas para poder cargar los datos correctamente

--- social/models.py
+++ social/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)#Conectaos esta fila a la tabla Usuarios de los modelos por defecto de django
+
+	def __str__(self):
+		return f'Perfil de {self.user.username}' #definimos una cadena de caracteres para poder ser mas visual en el admin de django
+
+class Post(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') #con el related name asociamos todos los posts que tiene un usuario, por asi decirlo es como añadir una fila post a la tabla de usuarios
+	timestamp = models.DateTimeField(default=timezone.now) #fecha de creacion del post
+	content = models.TextField()
+
+	class Meta:
+		ordering = ['-timestamp'] #Clase meta sirve para saber como se comporta la clase Post, con esto ordenamos Post, el - sirve àra que se cree en orden ascendente
+
+	def __str__(self):
+		return f'{self.user.username}: {self.content}' # aqui identificamos el usuario y el post que hizo el usuario , para facilitar el visionado de la tabla
--- social/views.py
+++ social/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def feed(request):
-	return render(request, 'social/feed.html')
+	posts = Post.objects.all()
+	context = {'posts': posts}
+	return render(request, 'social/feed.html', context)
 
 def profile(request):
 	return render(request, 'social/profile.html')
